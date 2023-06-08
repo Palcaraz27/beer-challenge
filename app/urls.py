a67bedbb795bef5baaedee87dbe7447b6d4a9e01
pedroAlcaraz <paj@certainly.io>
2023-06-08 16:02:55 +0200

    feat(beer-challenge): add endpoint to access swagger

--- app/urls.py
+++ app/urls.py
@@ -14,7 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Beer Challenge",
+        default_version='v1',
+        description="Beer Challenge with DDD and TDD",
+        contact=openapi.Contact(email="alcarazjoseph.pedro@gmail.com"),
+    ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
-    path('', include('api.urls'))
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', include('api.urls')),
 ]
