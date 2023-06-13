f6e7f48b1ed56c2c23aece55f0bda8919fee233c
pedroAlcaraz <paj@certainly.io>
2023-06-13 17:08:55 +0200

    feat: add beer domain errors

---
+++ core/festival/domain/errors.py
@@ -0,0 +1,16 @@
+from core.utils.models import DomainError
+
+
+class BeerError(DomainError):
+    pass
+
+class InvalidBeerIdError(BeerError):
+    def __str__(self) -> str:
+        return "The id is not valid."
+
+class BeerNameLenghtRuleError(BeerError):
+    def __init__(self) -> None:
+            super().__init__(message=f"The length of the name must be between a min and max value.")
+
+    def __str__(self) -> str:
+        return self.message
