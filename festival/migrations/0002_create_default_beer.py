d6d05a2ef40faed40ee0a5b6bc9f1ed4b8f66e7b
pedroAlcaraz <paj@certainly.io>
2023-06-14 16:17:48 +0200

    feat: add create default beer migration

---
+++ festival/migrations/0002_create_default_beer.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+
+def create_default_beer(apps, schema_editor):
+    Beer = apps.get_model('festival', 'Beer')
+    Beer.objects.create(name='Estrella Levante', price=1)
+
+def remove_default_beer(apps, schema_editor):
+    Beer = apps.get_model('festival', 'Beer')
+    instance = Beer.objects.get(id=1)
+    instance.delete()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('festival', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_default_beer, reverse_code=remove_default_beer),
+    ]
