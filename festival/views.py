f431c98aa3699abe49d94730c7d26214f47fdab8
pedroAlcaraz <paj@certainly.io>
2023-06-19 18:42:10 +0200

    feat: add delete beer endpoint

--- festival/views.py
+++ festival/views.py
@@ -8,7 +8,7 @@ from rest_framework.views import APIView
 from result import Err
 
 from app.cqrs.dispatcher import command_bus, query_bus
-from core.festival.application.command.create_beer_command import CreateBeerCommand
+from core.festival.application.command import CreateBeerCommand, RemoveBeerCommand
 from core.festival.application.query import GetBeerByIdQuery, GetBeersQuery
 from festival.serializers import RequestBeerSerializer
 
@@ -44,6 +44,17 @@ class BeerView(APIView):
         logger.info("Successful beers request.")
         return Response({"success": True, "beers": [store.to_json() for store in response.ok()]}, status=status.HTTP_200_OK)
 
+    def delete(self, request) -> Response:
+        body = json.loads(request.body)
+        beer_id = body.get("id", "")
+        response = command_bus.dispatch(RemoveBeerCommand(id=beer_id))
+
+        if isinstance(response, Err):
+            logger.warning("Error removing beers: {error}".format(error=response.err().message))
+            return Response(response.err().message, status=status.HTTP_400_BAD_REQUEST)
+
+        logger.info("Beer removed: {id}".format(id=beer_id))
+        return Response({"success": True}, status=status.HTTP_204_NO_CONTENT)
 
 class BeerByIdView(APIView):
     def get(self, request, id) -> Response:
