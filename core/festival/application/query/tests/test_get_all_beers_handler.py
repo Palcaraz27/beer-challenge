acccb2fca8e130d4bad857ee38c924b9626ad701
pedroAlcaraz <paj@certainly.io>
2023-06-19 16:05:58 +0200

    test: add get all beers query test

---
+++ core/festival/application/query/tests/test_get_all_beers_handler.py
@@ -0,0 +1,23 @@
+import pytest
+from result import Ok
+
+from core.festival.application.query.get_beers_query import GetBeersQuery
+from core.festival.application.query.get_beers_query_handler import GetBeersQueryHandler
+from core.festival.infrastructure.services.inmemory_beer_repository import InMemoryBeerRepository
+from core.festival.tests.beer_builder import BeerBuilder
+
+
+
+@pytest.mark.asyncio
+async def test_create_beer_success() -> None:
+    # Arrange
+    beer = BeerBuilder().build()
+    beer_repository = InMemoryBeerRepository(beers=[beer])
+    handler = GetBeersQueryHandler(beer_repository)
+    query = GetBeersQuery()
+
+    # Act
+    result = await handler.handle(query)
+
+    # Assert
+    assert isinstance(result, Ok)
