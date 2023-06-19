f60b882d5a420a000156417ede4570dbd756c2da
pedroAlcaraz <paj@certainly.io>
2023-06-19 17:38:06 +0200

    feat: registry get beer by id

--- app/cqrs/beer.py
+++ app/cqrs/beer.py
@@ -2,6 +2,8 @@ from typing import Dict
 
 from core.festival.application.command.create_beer_command import CreateBeerCommand
 from core.festival.application.command.create_beer_command_handler import CreateBeerCommandHandler
+from core.festival.application.query.get_beer_by_id_query import GetBeerByIdQuery
+from core.festival.application.query.get_beer_by_id_query_handler import GetBeerByIdQueryHandler
 from core.festival.application.query.get_beers_query import GetBeersQuery
 from core.festival.application.query.get_beers_query_handler import GetBeersQueryHandler
 from core.festival.infrastructure.services.django_beer_repository import DjangoBeerRepository
@@ -18,6 +20,9 @@ command_handlers: Dict[Command, CommandHandler] = {
 
 query_handlers: Dict[Query, QueryHandler] = {
     GetBeersQuery: GetBeersQueryHandler(
+        beer_repository=django_beer_repository
+    ),
+    GetBeerByIdQuery: GetBeerByIdQueryHandler(
         beer_repository=django_beer_repository
     ),
 }
