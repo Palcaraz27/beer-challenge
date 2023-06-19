a1e991f4cffd643e08b5b6e9da96a20d126f951b
pedroAlcaraz <paj@certainly.io>
2023-06-19 17:38:52 +0200

    feat: add new get beer by id endpoint

--- festival/urls.py
+++ festival/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from festival.views import BeerView
+from festival.views import BeerView, BeerByIdView
 
 app_name = "festival"
 
 urlpatterns = [
+    path("beer/<uuid:id>", BeerByIdView.as_view(), name="beer CRUD"),
     path("beer", BeerView.as_view(), name="beer CRUD"),
 ]
