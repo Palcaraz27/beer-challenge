3df8c8c1bee0ef76c7f7247d44bbc57e68384c36
pedroAlcaraz <paj@certainly.io>
2023-06-19 16:07:23 +0200

    test: add get all method in repositories

--- core/festival/infrastructure/services/tests/test_django_beer_repository.py
+++ core/festival/infrastructure/services/tests/test_django_beer_repository.py
@@ -1,19 +1,33 @@
+from django.test import TestCase
 import pytest
 
 from core.festival.tests.beer_builder import BeerBuilder
 from ..django_beer_repository import DjangoBeerRepository
 
 
-@pytest.mark.asyncio
-@pytest.mark.django_db(transaction=True)
-async def test_save_success() -> None:
-    # Arrange
-    django_repository = DjangoBeerRepository()
-    beer = BeerBuilder().build()
+class DjangoRepositoryTestCase(TestCase):
+    @pytest.mark.asyncio
+    async def test_save_success(self) -> None:
+        # Arrange
+        django_repository = DjangoBeerRepository()
+        beer = BeerBuilder().build()
 
-    # Act
-    await django_repository.save(beer)
+        # Act
+        await django_repository.save(beer)
 
-    # Assert
-    beer_found = await django_repository.get_by_id(beer_id=beer.beer_id)
-    assert beer_found
+        # Assert
+        beer_found = await django_repository.get_by_id(beer_id=beer.beer_id)
+        assert beer_found
+
+
+    @pytest.mark.asyncio
+    async def test_get_all_success(self) -> None:
+        # Arrange
+        django_repository = DjangoBeerRepository()
+
+        # Act
+        result = await django_repository.get_all()
+
+        # Assert
+        assert len(result) == 1
+        assert result[0].name.value == "Estrella Levante"
--- core/festival/infrastructure/services/tests/test_inmemory_beer_repository.py
+++ core/festival/infrastructure/services/tests/test_inmemory_beer_repository.py
@@ -17,3 +17,16 @@ async def test_save_new_beer() -> None:
     beer_found = await repository.get_by_id(beer_id=beer.beer_id)
     assert beer_found
     assert beer_found.beer_id == beer.beer_id
+
+@pytest.mark.asyncio
+async def test_get_all_beers() -> None:
+    # Arrange
+    beer = BeerBuilder().build()
+    repository = InMemoryBeerRepository(beers=[beer])
+
+    # Act
+    result = await repository.get_all()
+
+    # Assert
+    assert result
+    assert result[0] == beer
