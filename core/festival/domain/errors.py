30b025361b343455b97144915363e360b5544fed
pedroAlcaraz <paj@certainly.io>
2023-06-20 17:21:47 +0200

    feat: add dispenser business rules errors

--- core/festival/domain/errors.py
+++ core/festival/domain/errors.py
@@ -1,6 +1,7 @@
 from app.cqrs.models import DomainError
 
 
+# region: Beer Errors
 class BeerError(DomainError):
     pass
 
@@ -27,3 +28,40 @@ class BeerPriceRuleError(BeerError):
 class BeerNotFoundError(BeerError):
     def __init__(self, beer_id: str) -> None:
         super().__init__(message=f"Beer with id {beer_id} not found.")
+
+# endregion
+
+# region: Dispenser Errors
+class DispenserError(DomainError):
+    pass
+
+
+class InvalidDispenserIdError(DispenserError):
+    def __str__(self) -> str:
+        return "The id is not valid."
+
+
+class DispenserFlowVolumeRuleError(DispenserError):
+    def __init__(self) -> None:
+            super().__init__(message=f"Flow volume must be greater than 0.")
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class DispenserIsOpenRuleError(DispenserError):
+    def __init__(self) -> None:
+            super().__init__(message=f"The dispenser is already open.")
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class DispenserIsCloseRuleError(DispenserError):
+    def __init__(self) -> None:
+            super().__init__(message=f"The dispenser is already close.")
+
+    def __str__(self) -> str:
+        return self.message
+
+# endregion
