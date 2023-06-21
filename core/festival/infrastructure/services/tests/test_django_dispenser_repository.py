b2a44dcece2224259466498d598d1a49fa353736
pedroAlcaraz <paj@certainly.io>
2023-06-21 16:04:08 +0200

    feat: add get all dispensers django repository

--- core/festival/infrastructure/services/tests/test_django_dispenser_repository.py
+++ core/festival/infrastructure/services/tests/test_django_dispenser_repository.py
@@ -1,21 +1,28 @@
 import pytest
 from django.test import TestCase
-from core.festival.infrastructure.services.django_beer_repository import DjangoBeerRepository
+from asgiref.sync import async_to_sync
 
+from core.festival.infrastructure.services.django_beer_repository import DjangoBeerRepository
 from core.festival.infrastructure.services.django_dispenser_repository import DjangoDispenserRepository
 from core.festival.tests.beer_builder import BeerBuilder
 from core.festival.tests.dispenser_builder import DispenserBuilder
 
 
 class DjangoRepositoryTestCase(TestCase):
+    def setUp(self):
+        async def async_setup():
+            self.beer_repository = DjangoBeerRepository()
+            self.beer = BeerBuilder().build()
+            await self.beer_repository.save(self.beer)
+
+        async_to_sync(async_setup)()
+        
+
     @pytest.mark.asyncio
     async def test_save_success(self) -> None:
         # Arrange
-        beer_repository = DjangoBeerRepository()
-        beer = BeerBuilder().build()
         dispenser_repository = DjangoDispenserRepository()
-        dispenser = DispenserBuilder().build_with_beer(beer.beer_id.value)
-        await beer_repository.save(beer)
+        dispenser = DispenserBuilder().build_with_beer(self.beer.beer_id.value)
 
         # Act
         await dispenser_repository.save(dispenser)
@@ -23,3 +30,16 @@ class DjangoRepositoryTestCase(TestCase):
         # Assert
         dispenser_found = await dispenser_repository.get_by_id(dispenser.dispenser_id)
         assert dispenser_found
+
+    @pytest.mark.asyncio
+    async def test_get_all_success(self) -> None:
+        # Arrange
+        django_repository = DjangoDispenserRepository()
+        dispenser = DispenserBuilder().build_with_beer(self.beer.beer_id.value)
+        await django_repository.save(dispenser)
+
+        # Act
+        result = await django_repository.get_all()
+
+        # Assert
+        assert len(result) == 1
