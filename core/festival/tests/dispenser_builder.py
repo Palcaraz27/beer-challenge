ac2f47ef5fca6e1aa8302ad80451ad6ee37e6310
pedroAlcaraz <paj@certainly.io>
2023-06-21 13:24:25 +0200

    feat: add build with beer to dispenser builder

--- core/festival/tests/dispenser_builder.py
+++ core/festival/tests/dispenser_builder.py
@@ -15,14 +15,21 @@ class DispenserBuilder:
     _open_time: Optional[datetime]
 
     def __init__(self) -> None:
-        self._id = "193b926f-4d7a-4b79-9b4c-1128e994a85e",
-        self._beer_id = "123e4567-e89b-12d3-a456-526655440012",
+        self._id = "aa883fff-7d26-476d-becb-43d7101468f9"
+        self._beer_id = "123e4567-e89b-12d3-a456-526655440012"
         self._flow_volume = 1
 
     def build(self) -> Dispenser:
         return Dispenser(
             id=DispenserId(self._id),
             beer_id=BeerId(self._beer_id),
+            flow_volume=self._flow_volume
+        )
+
+    def build_with_beer(self, beer_id: str) -> Dispenser:
+        return Dispenser(
+            id=DispenserId(self._id),
+            beer_id=BeerId(beer_id),
             flow_volume=self._flow_volume
         )
 
