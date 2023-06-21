5312958f4c6b693eb8274fe9a669dd2ad21d6013
pedroAlcaraz <paj@certainly.io>
2023-06-21 13:23:49 +0200

    feat: add logger to beer django repository

--- core/festival/infrastructure/services/django_beer_repository.py
+++ core/festival/infrastructure/services/django_beer_repository.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List, Optional
 
 from core.festival.domain.beer import Beer, BeerId
@@ -6,12 +7,15 @@ from core.festival.infrastructure.services.django_beer_mapper import DjangoBeerM
 from festival.models import Beer as BeerModel
 
 
+logger = logging.getLogger(__name__)
+
+
 class DjangoBeerRepository(BeerRepository):
     def __init__(self) -> None:
         self.mapper = DjangoBeerMapper()
 
     async def save(self, beer: Beer) -> None:
-        original_instance = await self._get_by_uuid(beer.beer_id.value)
+        original_instance = await self.get_by_uuid(beer.beer_id.value)
 
         if original_instance is None:
             return await self._create_beer(beer)
@@ -19,25 +23,26 @@ class DjangoBeerRepository(BeerRepository):
         return original_instance
 
     async def get_by_id(self, beer_id: BeerId) -> Optional[Beer]:
-        instance = await self._get_by_uuid(beer_uuid=beer_id.value)
+        instance = await self.get_by_uuid(beer_uuid=beer_id.value)
         return self.mapper.to_domain(instance) if instance else None
 
     async def get_all(self) -> List[Beer]:
         return [self.mapper.to_domain(beer) async for beer in BeerModel.objects.all()]
 
     async def delete(self, beer: Beer) -> None:
-        original_instance = await self._get_by_uuid(beer.beer_id.value)
+        original_instance = await self.get_by_uuid(beer.beer_id.value)
 
         if original_instance is None:
             return original_instance
 
         await self._remove_beer(beer=original_instance)
 
-    async def _get_by_uuid(self, beer_uuid: str) -> Optional[BeerModel]:
+    async def get_by_uuid(self, beer_uuid: str) -> Optional[BeerModel]:
         try:
             instance = await BeerModel.objects.aget(uuid=beer_uuid)
             return instance
         except BeerModel.DoesNotExist:
+            logger.warning("Error getting beer: {uuid} does not exist".format(uuid=beer_uuid))
             return None
 
     async def _create_beer(self, beer: Beer) -> None:
