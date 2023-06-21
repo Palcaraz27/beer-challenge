0cc95d0c3df8d0eefe6ae14193acb60b1874ee71
pedroAlcaraz <paj@certainly.io>
2023-06-21 16:06:53 +0200

    test: add get all dispensers query handler

---
+++ core/festival/application/query/tests/test_get_all_dispensers_handler.py
@@ -0,0 +1,21 @@
+import pytest
+from result import Ok
+
+from core.festival.application.query import GetDispensersQuery, GetDispensersQueryHandler
+from core.festival.infrastructure.services.inmemory_dispenser_repository import InMemoryDispenserRepository
+from core.festival.tests.dispenser_builder import DispenserBuilder
+
+
+@pytest.mark.asyncio
+async def test_get_all_dispensers_success() -> None:
+    # Arrange
+    dispenser = DispenserBuilder().build()
+    beer_repository = InMemoryDispenserRepository(dipensers=[dispenser])
+    handler = GetDispensersQueryHandler(beer_repository)
+    query = GetDispensersQuery()
+
+    # Act
+    result = await handler.handle(query)
+
+    # Assert
+    assert isinstance(result, Ok)
